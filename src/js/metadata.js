698266cc5ea794d5a15d78a30ede832ddf43bc2a
steady eddie <ed.ag.ramirez@hotmail.com>
2026-04-03 19:44:00 +0100

    JS refactor failed
    Revert "refactor"
    
    This reverts commit 606696e8a1a5ccc73cbc378857ca7f98c19189d9.

--- src/js/metadata.js
+++ src/js/metadata.js
@@ -3,14 +3,13 @@ export function loadSystemData() {
     const now = new Date();
     
     // Update Time/Date
-    document.getElementById('current-date').textContent = `Date: ${now.toDateString()}`;
-    document.getElementById('current-time').textContent = `Time: ${now.toLocaleTimeString()}`;
+    document.getElementById('current-date')?.textContent = `Date: ${now.toDateString()}`;
+    document.getElementById('current-time')?.textContent = `Time: ${now.toLocaleTimeString()}`;
 
     // Update Hardware Info
-    document.getElementById('timezone').textContent = `Timezone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}`;
-    document.getElementById('screen-width').textContent = `Screen Width: ${window.screen.width}`;
-    document.getElementById('screen-height').textContent = `Screen Height: ${window.screen.height}`;
-    document.getElementById('user-agent').textContent = `User Agent: ${navigator.userAgent}`;
-
+    document.getElementById('timezone')?.textContent = `Timezone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}`;
+    document.getElementById('screen-width')?.textContent = `Screen Width: ${window.screen.width}`;
+    document.getElementById('user-agent')?.textContent = `User Agent: ${navigator.userAgent}`;
+    
     console.log("SYSTEM: META-DATA LOGGED");
 }
